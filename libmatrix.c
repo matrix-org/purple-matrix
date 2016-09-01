14d4b32f3ba400f26c9099fbfdaddf4ebc34c68e
Dr. David Alan Gilbert <dave@treblig.org>
2016-09-01 01:54:25 +0100

    Send inserted images
    Turn on purple's image facility and cause the messages to be sent.
    
    There are two tricky parts to this:
      a) Purple's idea is a message that can include an image anywhere
         in it; while Matrix's idea is that you have text events and image
         events but currently no combo events.  My (hacky) solution here
         is to split purple messages into the text and image components.
    
      b) Matrix's images are files that need uploading before the event
         is sent, and the upload process gives a uri back that must
         be included in the event.  That means we have to somehow delay
         sending the event until we have the file uploaded.  I solve that
         by using the event hook I added in an earlier commit.
    
    Signed-off-by: Dr. David Alan Gilbert <dave@treblig.org>

--- libmatrix.c
+++ libmatrix.c
@@ -230,7 +230,6 @@ static char *matrixprpl_get_cb_real_name(PurpleConnection *gc, int id,
 }
 
 
-
 /******************************************************************************
  *
  * prpl stuff. see prpl.h for more information.
@@ -238,7 +237,8 @@ static char *matrixprpl_get_cb_real_name(PurpleConnection *gc, int id,
 
 static PurplePluginProtocolInfo prpl_info =
 {
-    OPT_PROTO_UNIQUE_CHATNAME | OPT_PROTO_CHAT_TOPIC,    /* options */
+    OPT_PROTO_UNIQUE_CHATNAME | OPT_PROTO_CHAT_TOPIC |
+      OPT_PROTO_IM_IMAGE,    /* options */
     NULL,               /* user_splits, initialized in matrixprpl_init() */
     NULL,               /* protocol_options, initialized in matrixprpl_init() */
     {   /* icon_spec, a PurpleBuddyIconSpec */
