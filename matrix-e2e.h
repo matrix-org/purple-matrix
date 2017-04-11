69a05a3f36e1d364217d1ddb8c0bae3dc4e549b6
Dr. David Alan Gilbert <dave@treblig.org>
2017-04-11 01:01:45 +0100

    e2e: Decrypt m.room.encrypted
    Decrypt m.room.encrypted messages sent to a room;
    these come out with normal events.
    
    Signed-off-by: Dr. David Alan Gilbert <dave@treblig.org>

--- matrix-e2e.h
+++ matrix-e2e.h
@@ -23,8 +23,10 @@
 #include "matrix-connection.h"
 
 typedef struct _MatrixE2EData MatrixE2EData;
+typedef struct _PurpleConversation PurpleConversation;
 
 int matrix_e2e_get_device_keys(MatrixConnectionData *conn, const gchar *device_id);
 void matrix_e2e_decrypt_d2d(struct _PurpleConnection *pc, struct _JsonObject *event);
+void matrix_e2e_decrypt_room(struct _PurpleConversation *conv, struct _JsonObject *event);
 
 #endif
